7362ab95ac4d579eb45c872abfc6a92bbe38691f
zadock <kinara.zadock@gmail.com>
2019-07-10 14:49:05 +0300

    feat: add eventlistener to toggle-button

--- shared.js
+++ shared.js
@@ -1,12 +1,23 @@
 var backdrop = document.querySelector('.backdrop');
-
 var selectPlanButton = document.querySelectorAll('.plan button');
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
 
-console.log(selectPlanButton);
+
 
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener('click', function() {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
+}
+
+
+backdrop.addEventListener("click", closeModal);
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";  
 }
