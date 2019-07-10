6f9e2e9efc63d482037e92616a99dcc92e3c2e60
zadock <kinara.zadock@gmail.com>
2019-07-10 15:45:06 +0300

    fix: update js to use .classList()

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var selectPlanButton = document.querySelectorAll('.plan button');
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var  toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 
 
@@ -9,15 +11,30 @@ for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener('click', function() {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
+
+  // modal.className = 'open';   THIS WILL OVERWRITE THE COMPLETE CLASS IN THE ELEMENT
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function(){
+  mobileNav.style.display = 'none';
+  closeModal();
+} );
 
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";  
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";  
+
+  modal.classList.remove('open');
+  backdrop.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', function(){
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
