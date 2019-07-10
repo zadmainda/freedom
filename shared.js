f34c1e63974730897049a88c8b6cf6f6783b1d68
zadock <kinara.zadock@gmail.com>
2019-07-10 17:09:00 +0300

    fix: add truthy/falsy function to js to cater for pages without modal

--- shared.js
+++ shared.js
@@ -26,15 +26,22 @@ backdrop.addEventListener("click", function(){
   closeModal();
 } );
 
-modalNoButton.addEventListener("click", closeModal);
+
+if (modalNoButton){
+  modalNoButton.addEventListener("click", closeModal);
+}
+
 
 
 function closeModal() {
   // backdrop.style.display = "none";
   // modal.style.display = "none";  
 
-  modal.classList.remove('open');
-  backdrop.classList.remove('open');
+  if(modal){
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+  }
+
 }
 
 toggleButton.addEventListener('click', function(){
